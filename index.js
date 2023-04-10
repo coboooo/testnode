df10e5940c5f1a79c34d66fd84aa112e3adfc8d9
coboooo <50066465+coboooo@users.noreply.github.com>
2023-04-10 13:18:40 +0900

    1

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const express = require("express");
-const axios = require("axios");
-
 const app = express();
-const baseURL = "https://pokeapi.co/api/v2/";
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Pokemon API");
+  res.send("Hello, world!");
 });
 
-app.get("/pokemon", (req, res) => {
-  const limit = 151; // 가져올 포켓몬 수
-  axios
-    .get(`${baseURL}pokemon?limit=${limit}`)
-    .then((response) => {
-      const names = response.data.results.map((pokemon) => pokemon.name);
-      res.send({ names });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send(error);
-    });
-});
-
-// app.listen(3000, () => {
-//   console.log("Server running at http://localhost:3000/");
-// });
-
-//
+module.exports = app;
